c1e8c2caeb8c738065d4a9537c781c885e79c51f
jamiefeiss <jamie.feiss@gmail.com>
2021-06-11 10:02:11 +1000

    Added detailed validation error messages

--- scripts/validate_vocabs.py
+++ scripts/validate_vocabs.py
@@ -10,7 +10,7 @@ def main():
     assert r.status_code == 200
 
     # for all vocabs...
-    invalid_vocabs = []
+    invalid_vocabs = {} # format {vocab_filename: error_msg}
     vocabs_dir = Path(__file__).parent.parent / "vocabularies"
     for f in vocabs_dir.iterdir():
         # ...validate each file
@@ -18,18 +18,20 @@ def main():
             try:
                 v = validate(str(f), shacl_graph=r.text, shacl_graph_format="ttl")
                 if not v[0]:
-                    invalid_vocabs.append(f)
+                    invalid_vocabs[f.name] = v[2]
+            # syntax errors crash the validate() function
             except Exception as e:
-                print(e)
-                invalid_vocabs.append(f)
+                invalid_vocabs[f.name] = e
 
     # check to see if we have any invalid vocabs
-    if len(invalid_vocabs) > 0:
-        print("Invalid Vocabs:")
-        for iv in invalid_vocabs:
-            print("\t- {}".format(iv))
+    if len(invalid_vocabs.keys()) > 0:
+        print("Invalid Vocabs:\n")
+        for vocab, error in invalid_vocabs.items():
+            print(f"- {vocab}:")
+            print(error)
+            print("-----")
 
-    assert len(invalid_vocabs) == 0, "Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs]))
+    assert len(invalid_vocabs.keys()) == 0, "Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs.keys()]))
 
 
 if __name__ == "__main__":
